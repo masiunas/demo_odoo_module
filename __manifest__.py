35ba3a1ad2d7add2affa16d5cdc568099661f2b7
masiunas <silencer368@gmail.com>
2019-05-24 12:24:27 +0300

    Added icon module

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,7 @@
     'name': "Library Management",
     'description': "Manage library book catalogue and lending.",
     'author': "Evgenii Masiunas",
+    "licence": "By default considered to be LGPL",
     'category': "Management",
     'depends': ['base'],
     'application': True,
