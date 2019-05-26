c96661438b634737e645148c1a2e8a32ba2d337b
masiunas <silencer368@gmail.com>
2019-05-26 16:54:38 +0300

    Tested business logic and access security

--- tests/test_book.py
+++ tests/test_book.py
@@ -5,6 +5,8 @@ class TestBook(TransactionCase):
 
     def setUp(self, *args, **kwargs):
         result = super().setUp(*args, **kwargs)
+        user_admin = self.env.ref('base.user_admin')
+        self.env = self.env(user=user_admin)
         self.Book = self.env['library.book']
         self.book_ode = self.Book.create({
             'name': 'Odoo Development Essentials',
@@ -15,4 +17,6 @@ class TestBook(TransactionCase):
         """Test Books are active by default"""
         self.assertEqual(self.book_ode.active, True)
 
-
+    def test_check_isbn(self):
+        "Check valid ISBN"
+        self.assertTrue(self.book_ode._check_isbn)
