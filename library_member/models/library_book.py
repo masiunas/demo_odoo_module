4c73657a3130084397da37c2e36a2b8642c0c261
masiunas <silencer368@gmail.com>
2019-06-03 14:08:02 +0300

    Changed existing fields.

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -1,9 +1,8 @@
-from Odoo import fields, models
+from odoo
 
 
 class Book(models.Model):
     inherit = 'library.book'
     is_available = fields.Boolean('Is Available?')
-
-
-
+    isbn = fields.Char(help="Use a valid ISBN-13 or ISBN-10.")
+    publisher_id = fields.Many2one(index=True)
