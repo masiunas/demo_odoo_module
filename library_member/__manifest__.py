7423620f3c4f7403365f88988701fa2809043580
masiunas <silencer368@gmail.com>
2019-06-04 10:42:16 +0300

    Fixed errors.

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -3,6 +3,10 @@
     'name': 'Library Members',
     'description': 'Manage people who will be able to borrow books.',
     'author': 'Evgenii _Masiunas',
-    'depends': ['demo_library_module'],
+    'depends': ['library_module'],
+    'data': [
+        'views/book_view.xml',
+        'security/library_security.xml',
+    ],
     'application': False,
 }
--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 
 class Book(models.Model):
-    inherit = 'library.book'
+    _inherit = 'library.book'
     is_available = fields.Boolean('Is Available?')
     isbn = fields.Char(help="Use a valid ISBN-13 or ISBN-10.")
     publisher_id = fields.Many2one(index=True)
