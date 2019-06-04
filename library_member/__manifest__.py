b6899e4a8143e1fa03129391faa676cc9dd2e527
masiunas <silencer368@gmail.com>
2019-06-04 14:01:04 +0300

    Embedded Models using delegation inheritance.

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -6,7 +6,11 @@
     'depends': ['library_module'],
     'data': [
         'views/book_view.xml',
+        'security/ir.model.access.csv',
         'security/library_security.xml',
+        'views/member_view.xml',
+        'views/library_menu.xml',
+
     ],
     'application': False,
 }
